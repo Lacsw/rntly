9f9a33df801199edd62145287a98620e53248d7f
Roman Frolov <roman.frolov@purpose-green.com>
2025-12-15 20:02:08 +0100

    ref: create response module

---
+++ internal/response/json.go
@@ -0,0 +1,20 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+func Error(w http.ResponseWriter, status int, message string) {
+	JSON(w, status, map[string]string{"error": message})
+}
+
+func NoContent(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNoContent)
+}
